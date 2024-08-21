3f4314176f40a3b1e8034724ef648cf82f585c9f
Manan-Painter <155348139+Manan-Painter@users.noreply.github.com>
2024-08-21 11:36:56 +0530

    some change

--- src/components/Feature.tsx
+++ src/components/Feature.tsx
@@ -1,11 +1,88 @@
-import React from 'react'
+'use client'
 
-const Feature = () => {
-  return (
-    <div>
-      Feature
+import data from '@/data' 
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+
+const Featured = () => {
+  // var settings = {
+  //   dots: true,
+  //   infinite: false,
+  //   speed: 100,
+  //   slidesToShow: 4,
+  //   slidesToScroll: 1,
+  //   initialSlide: 0,
+  //   responsive: [
+  //     {
+  //       breakpoint: 1024,
+  //       settings: {
+  //         slidesToShow: 3,
+  //         slidesToScroll: 3,
+  //         infinite: true,
+  //         dots: true
+  //       }
+  //     },
+  //     {
+  //       breakpoint: 600,
+  //       settings: {
+  //         slidesToShow: 2,
+  //         slidesToScroll: 2,
+  //         initialSlide: 2
+  //       }
+  //     },
+  //     {
+  //       breakpoint: 480,
+  //       settings: {
+  //         slidesToShow: 1,
+  //         slidesToScroll: 1
+  //       }
+  //     }
+  //   ]
+  // };
+
+  // const settings = {
+  //   dots: true,
+  //   infinite: true,
+  //   slidesToShow: 3,
+  //   slidesToScroll: 1,
+  //   autoplay: true,
+  //   speed: 2000,
+  //   autoplaySpeed: 2000,
+  //   cssEase: "linear"
+  // };
+  var settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    pauseOnHover: true
+  };
+  return ( 
+    <div className="w-5/6 m-auto">
+      <div className="mt-5">
+      <Slider {...settings}>
+        {
+          data.map((e:any) => (
+            <div className='bg-white h-[500px] text-black rounded-lg transform transition duration-500 hover:scale-95 hover:shadow-lg'>
+              <div className='h-56 rounded-t-xl bg-orange-600 flex justify-center items-center'>
+                <img src={e.img} alt="" className='h-44 w-44 rounded-full' />
+              </div>
+              <div className='flex flex-col justify-center items-center gap-4 p-4'>
+                <p className='text-xl font-semibold'>{e.title}</p>
+                <p>{e.desc}</p>
+                <button className='bg-orange-600 text-white text-lg px-6 py-1 rounded-xl'>{e.price}</button>
+              </div>  
+            </div>
+          ))
+        }
+        </Slider>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Feature
+export default Featured;
--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,76 +1,64 @@
 "use client";
-
-import React, { useState, useEffect } from "react";
 import Image from "next/image";
-import burgerImage from "../../public/images/nurger9.png";
-import pizza from '../../public/images/pizza.jpg'
+import React, { useEffect, useState } from "react";
+
+const data = [
+  {
+    id: 1,
+    title: "always fresh & always crispy & always hot",
+    image: "/images/burger.avif",
+  },
+  {
+    id: 2,
+    title: "we deliver your order wherever you are in NY",
+    image: "/images/bread.avif",
+  },
+  {
+    id: 3,
+    title: "the best pizza to share with your family",
+    image: "/images/pizza.avif",
+  },
+];
+
 
 const Slider = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const images = [
-    burgerImage,
-    pizza,
-    "https://mdbcdn.b-cdn.net/img/new/slides/043.webp",
-  ];
+  const [currentSlide, setCurrentSlide] = useState(0);
 
   useEffect(() => {
-    const intervalId = setInterval(() => {
-      setCurrentIndex((prevIndex) =>
-        prevIndex === images.length - 1 ? 0 : prevIndex + 1
-      );
-    }, 3000); // Change the interval time to 3000ms (3 seconds)
-
-    return () => clearInterval(intervalId); // Cleanup interval on component unmount
-  }, [images.length]);
-
-  const handlePrev = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    const interval = setInterval(
+      () =>
+        setCurrentSlide((prev) => (prev === data.length - 1 ? 0 : prev + 1)),
+      4000
     );
-  };
-
-  const handleNext = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex === images.length - 1 ? 0 : prevIndex + 1
-    );
-  };
+    return () => clearInterval(interval);
+  }, []);
 
   return (
-   
-    <div className="relative">
-      <button
-        className="absolute top-1/2 left-4 transform -translate-y-1/2 bg-gray-800 text-white p-2 rounded-full shadow-lg hover:bg-gray-700 transition"
-        onClick={handlePrev}
-      >
-        &lt;
-      </button>
-      <div className="overflow-hidden">
-        <div
-          className="flex transition-transform duration-500 ease-in-out"
-          style={{ transform: `translateX(-${currentIndex * 100}%)` }}
-        >
-          {images.map((src, index) => (
-            <div key={index} className="w-full flex-shrink-0">
-              <Image
-                src={src}
-                alt={`Slide ${index + 1}`}
-                className="w-full h-60 object-cover"
-                layout="responsive"
-                width={800}
-                height={600}
-              />
-            </div>
-          ))}
-        </div>
+    <div className="flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] lg:flex-row bg-orange-600 p-5 ">
+      <div className="w-full flex-1 relative  rounded-xl  overflow-hidden ">
+        <Image
+          src={data[currentSlide].image}
+          alt=""
+          fill
+          className="object-cover rounded-3xl border-4 border-white  transform transition duration-500 hover:scale-110 hover:shadow-lg"
+        />
       </div>
-      <button
-        className="absolute top-1/2 right-4 transform -translate-y-1/2 bg-gray-800 text-white p-2 rounded-full shadow-lg hover:bg-gray-700 transition"
-        onClick={handleNext}
-      >
-        &gt;
-      </button>
+      <div className="flex-1 flex items-center justify-center flex-col gap-8 text-white font-bold">
+  <h1 className="text-5xl text-center uppercase p-4 md:p-10 md:text-6xl xl:text-7xl transition-transform duration-300 hover:scale-105">
+    {data[currentSlide].title}
+  </h1>
+  <button className="bg-white text-orange-600 py-4 px-8  hover:bg-orange-400 rounded-lg  transition-transform duration-300 hover:scale-105">
+    Order Now
+  </button>
+</div>
+
+      
     </div>
   );
 };
+
+
+
+
 
 export default Slider;
