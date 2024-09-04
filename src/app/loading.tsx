af1e183672bbe24a9558cf6b48ff352645d3ff62
Manan-Painter <mananpainter056@gmail.com>
2024-09-04 18:09:39 +0530

    loader implement

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { TbLoader3 } from "react-icons/tb";
 
 const Loading = () => {
-  return <div>Loading...</div>;
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <TbLoader3 className="animate-spin text-6xl text-orange-500" />
+    </div>
+  );
 };
 
 export default Loading;
